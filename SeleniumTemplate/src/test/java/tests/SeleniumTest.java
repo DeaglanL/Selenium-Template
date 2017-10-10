8dbe4bf9dac6b42b0e6401386d53d238ed61dbb3
Deaglan Lynch <Deaglanlynch@hotmail.co.uk>
2017-10-10 13:27:45 +0000

    set up report class

--- SeleniumTemplate/src/test/java/tests/SeleniumTest.java
+++ SeleniumTemplate/src/test/java/tests/SeleniumTest.java
@@ -1,6 +1,6 @@
 package tests;
 
-        import org.junit.*;
+import org.junit.*;
 
 
 public class SeleniumTest {
--- SeleniumTemplate/src/test/java/util/Reports.java
+++ SeleniumTemplate/src/test/java/util/Reports.java
@@ -1,4 +1,40 @@
 package util;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Reports {
+
+    private ExtentReports report;
+
+    public Reports(String reportName, String reportTitle, String reportFilePath){
+        report = new ExtentReports();
+        ExtentHtmlReporter extentHtmlReporter = new ExtentHtmlReporter(reportFilePath);
+        extentHtmlReporter.config().setReportName(reportName);
+        extentHtmlReporter.config().setDocumentTitle(reportTitle);
+        report.attachReporter(extentHtmlReporter);
+    }
+
+    public Map<Integer, ExtentTest> createReportTests(String[] testNames){
+        Map<Integer, ExtentTest> tests = new HashMap<>();
+
+        for (int i = 0; i < testNames.length; i++) {
+
+            tests.put(i, report.createTest(testNames[i]));
+
+        }
+        return tests;
+    }
+
+
+    public void closeReport(){
+        report.flush();
+    }
+
+
+
 }
